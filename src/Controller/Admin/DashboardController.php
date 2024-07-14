ffcab64d18b7b694ae586431a2644caad6e393f1
soulweby <souleyso47@gmail.com>
2024-07-14 20:48:30 +0200

    {AuthEntity}

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,9 +2,15 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Article;
+use App\Entity\Categorie;
+use App\Entity\Comment;
+use App\Entity\Image;
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,18 +21,27 @@ class DashboardController extends AbstractDashboardController
      */
     public function index(): Response
     {
-        return parent::index();
+        // return parent::index();
+
+        $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+        return $this->redirect($adminUrlGenerator->setController(ArticleCrudController::class)->generateUrl());
     }
 
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Livres Debat');
+            ->setTitle('Livres et Débats');
     }
 
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::section('Blog');
+        yield MenuItem::linkToCrud('Articles', 'fa fa-file-text', Article::class);
+        yield MenuItem::linkToCrud('Categories', 'fa fa-tags', Categorie::class);
+        yield MenuItem::linkToCrud('Media', 'fa fa-image', Image::class);
+        yield MenuItem::section('Users');
+        yield MenuItem::linkToCrud('Utilisateurs', 'fa fa-user', User::class);
+        yield  MenuItem::linkToCrud('Commentaires', 'fa fa-comment', Comment::class);
     }
 }
---
+++ src/Entity/Article.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ArticleRepository::class)]
+class Article
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $titre;
+
+    #[ORM\Column(type: 'boolean', nullable: true)]
+    private $publication;
+
+    #[ORM\Column(type: 'text')]
+    private $content;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $autor;
+
+    #[ORM\Column(type: 'date')]
+    private $date;
+
+    #[ORM\OneToOne(inversedBy: 'article', targetEntity: Image::class, cascade: ['persist', 'remove'])]
+    private $Image;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function isPublication(): ?bool
+    {
+        return $this->publication;
+    }
+
+    public function setPublication(?bool $publication): self
+    {
+        $this->publication = $publication;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getAutor(): ?string
+    {
+        return $this->autor;
+    }
+
+    public function setAutor(string $autor): self
+    {
+        $this->autor = $autor;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getImage(): ?Image
+    {
+        return $this->Image;
+    }
+
+    public function setImage(?Image $Image): self
+    {
+        $this->Image = $Image;
+
+        return $this;
+    }
+}
